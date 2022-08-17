05534795ed98b4cda16ca517325db9bd3af91c28
kdongk <ldh5081@naver.com>
2022-08-17 17:21:01 +0900

    고객관리 구조화

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import logo from './logo.svg';
+import React, { component } from 'react';
 import './App.css';
+import Customer from "./components/Customer";
+
+const customers = [
+    {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '홍길동',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '대학생'
+    },
+    {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '이동현',
+        'birthday': '960630',
+        'gender': '남자',
+        'job': '대학생'
+    },
+    {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '김김김',
+        'birthday': '923423',
+        'gender': '여자',
+        'job': '대학생'
+    }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+    return (
+        <div>
+            {
+                customers.map(c => {
+                    return (
+                        <Customer
+                            key={c.id}
+                            id={c.id}
+                            image={c.image}
+                            name={c.name}
+                            birthday={c.name}
+                            gender={c.gender}
+                            job={c.job}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
