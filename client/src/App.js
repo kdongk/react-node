05a81eb879c0b8306d0ed1b75232756c8f9a75ef
kdongk <ldh5081@naver.com>
2022-08-17 20:46:22 +0900

    서버 정보 client로 가져가서 쓰기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Customer from "./components/Customer";
 
@@ -9,81 +9,62 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
-import { withStyles } from '@mui/material/styles';
 
-const styles = theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing.unit * 3,
-        overflowX: "auto"
-    },
-    table: {
-        minWidth: 1080
+// const styles = theme => ({
+//     root: {
+//         width: '100%',
+//         marginTop: theme.spacing.unit * 3,
+//         overflowX: "auto"
+//     },
+//     table: {
+//         minWidth: 1080
+//     }
+// });
+
+class App extends Component {
+
+    state = {
+        customers: ""
     }
-})
 
-const customers = [
-    {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '홍길동',
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '대학생'
-    },
-    {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '이동현',
-        'birthday': '960630',
-        'gender': '남자',
-        'job': '대학생'
-    },
-    {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '김김김',
-        'birthday': '923423',
-        'gender': '여자',
-        'job': '대학생'
+    componentDidMount() {
+        this.callApi()
+            // body값을 res에 넣고, 그걸
+            // 다시 customers라는 변수에 넣는다
+            .then(res => this.setState({customers: res}))
+            .catch(err => console.log(err));
     }
-]
 
-function App() {
-    //const { classes } = this.props;
-    return (
-        <Paper>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>번호</TableCell>
-                        <TableCell>이미지</TableCell>
-                        <TableCell>이름</TableCell>
-                        <TableCell>생년월일</TableCell>
-                        <TableCell>성별</TableCell>
-                        <TableCell>직업</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        customers.map(c => {
-                            return (
-                                <Customer
-                                    key={c.id}
-                                    id={c.id}
-                                    image={c.image}
-                                    name={c.name}
-                                    birthday={c.name}
-                                    gender={c.gender}
-                                    job={c.job}
-                                />
-                            );
-                        })
-                    }
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+    callApi = async () => {
+        // api/customers 경로에 목록을 json형태로 body한테 준다
+        const response = await fetch('/api/customers');
+        const body = await response.json();
+        return body;
+    }
+
+    render(){
+        return (
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.customers ? this.state.customers.map(c => {
+                            return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/>);
+                        }) : ""}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
 
 export default App;
